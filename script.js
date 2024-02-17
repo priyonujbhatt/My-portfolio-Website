b75dea432ccfc1c6c7f947bd2f99b93a678f5222
priyonujbhatt <106647911+priyonujbhatt@users.noreply.github.com>
2024-02-17 19:18:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+       nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+       nav.removeAttribute("style");
+   }
+} 
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  // Insert code to handle button click event here
+});
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
